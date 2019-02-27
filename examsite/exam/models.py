25567bacfa9dd02732d92809fd5c8eb4675608ef
Krystian <krystian.tomaszewski3@gmail.com>
2019-02-27 19:22:31 +0100

    First migrate, new branch, and models

--- examsite/exam/models.py
+++ examsite/exam/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Sheet(models.Model):
+    sheet_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sheet_name
+
+class Question(models.Model):
+    sheet = models.ForeignKey(Sheet, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text + '-' + str(self.votes)
