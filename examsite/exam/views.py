ee5ea4edc207bafc08cfe8c35617923576c5e0d5
Krystian <krystian.tomaszewski3@gmail.com>
2019-02-28 17:57:36 +0100

    Category

--- examsite/exam/views.py
+++ examsite/exam/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Sheet, Question, Choice
+
+
+
 
 # Create your views here.
+
+class SheetList(generic.ListView):
+    model = Sheet
+
+    def __str__(self):
+        return self.Sheet.name
+
+sheet_list = SheetList.as_view()
+
+class QuestionList(generic.ListView):
+    model = Question
+
+    def __str__(self):
+        return self.Question.name
+
+question_list = QuestionList.as_view()
+
+class ChoiceList(generic.ListView):
+    model = Choice
+
+    def __str__(self):
+        return self.Choice.name
+
+choice_list = ChoiceList.as_view()
+
